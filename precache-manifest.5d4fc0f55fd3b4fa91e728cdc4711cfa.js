3908c4e650c44c2af282f3e51b8a2fb55b305896
ritik <ritik-01>
2020-07-23 19:13:15 +0530

    Updates

---
+++ precache-manifest.5d4fc0f55fd3b4fa91e728cdc4711cfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbd3416d091ee5bf993281d2cea8f92",
+    "url": "/Facebook-login-Page/index.html"
+  },
+  {
+    "revision": "f3488f1534bedb4cb3dd",
+    "url": "/Facebook-login-Page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ae8cbf065ded1628ae3b",
+    "url": "/Facebook-login-Page/static/css/main.e245ad3b.chunk.css"
+  },
+  {
+    "revision": "f3488f1534bedb4cb3dd",
+    "url": "/Facebook-login-Page/static/js/2.72b8eccf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Facebook-login-Page/static/js/2.72b8eccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8cbf065ded1628ae3b",
+    "url": "/Facebook-login-Page/static/js/main.ad5d5c11.chunk.js"
+  },
+  {
+    "revision": "6f6baa40d721fb50ca7c",
+    "url": "/Facebook-login-Page/static/js/runtime-main.07cc0216.js"
+  }
+]);
